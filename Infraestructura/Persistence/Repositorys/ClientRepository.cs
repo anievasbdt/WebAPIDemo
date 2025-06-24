1177e112ba46cae8c48e46e9ecb6773a68465d88
anievasbdt <anievas@bdtglobal.com>
2025-06-24 14:03:31 -0300

    fix

--- Infraestructura/Persistence/Repositorys/ClientRepository.cs
+++ Infraestructura/Persistence/Repositorys/ClientRepository.cs
@@ -22,41 +22,32 @@ namespace Infraestructura.Persistence.Repositorys
             {
                 throw new ArgumentNullException(nameof(client), "Client cannot be null");
             }
-            // llamamos al StoredProcedure SP_INSERT_CLIENT
-            // que recibe 9 parametros:
-            /*
-            Comando.Parameters.Add("P_SCLIENT", OracleDbType.Char, Cliente.SCLIENT, ParameterDirection.Input)
-            Comando.Parameters.Add("P_DBIRTHDAT", OracleDbType.Date, Cliente.DBIRTHDAT, ParameterDirection.Input)
-            Comando.Parameters.Add("P_SFIRSTNAME", OracleDbType.Char, Cliente.SFIRSTNAME, ParameterDirection.Input)
-            Comando.Parameters.Add("P_SLASTNAME", OracleDbType.Char, Cliente.SLASTNAME, ParameterDirection.Input)
-            Comando.Parameters.Add("P_SLASTNAME2", OracleDbType.Char, Cliente.SLASTNAME2, ParameterDirection.Input)
-            Comando.Parameters.Add("P_SCUIT", OracleDbType.Char, Cliente.SCUIT, ParameterDirection.Input)
-            Comando.Parameters.Add("P_SLEGALNAME", OracleDbType.Char, Cliente.SLEGALNAME, ParameterDirection.Input)
-            Comando.Parameters.Add("P_SSEXCLIEN", OracleDbType.Char, Cliente.SSEXCLIEN, ParameterDirection.Input)
-            Comando.Parameters.Add("P_NNATIONALITY", OracleDbType.Char, Cliente.NNATIONALITY, ParameterDirection.Input)
-             */
-            OracleCommand command = new OracleCommand("user_insert", context.Database.GetDbConnection() as OracleConnection);
-            command.CommandType = CommandType.StoredProcedure;
-            command.Parameters.Add("P_SCLIENT", OracleDbType.Char, client.SClient, ParameterDirection.Input);
-            command.Parameters.Add("P_DBIRTHDAT", OracleDbType.Date, client.DBirthDat, ParameterDirection.Input);
-            command.Parameters.Add("P_SFIRSTNAME", OracleDbType.Char, client.SFirstName, ParameterDirection.Input);
-            command.Parameters.Add("P_SLASTNAME", OracleDbType.Char, client.SLastName, ParameterDirection.Input);
-            command.Parameters.Add("P_SLASTNAME2", OracleDbType.Char, client.SLastName2, ParameterDirection.Input);
-            command.Parameters.Add("P_SCUIT", OracleDbType.Char, client.SCuit, ParameterDirection.Input);
-            command.Parameters.Add("P_SLEGALNAME", OracleDbType.Char, client.SLegalName, ParameterDirection.Input);
-            command.Parameters.Add("P_SSEXCLIEN", OracleDbType.Char, client.SSexClien, ParameterDirection.Input);
-            command.Parameters.Add("P_NNATIONALITY", OracleDbType.Int32, client.NNationality, ParameterDirection.Input);
-            // ahora ejecutamos el comando
             try
             {
+                context.Database.OpenConnection();
+                OracleCommand command = new OracleCommand("SP_INSERT_CLIENT", context.Database.GetDbConnection() as OracleConnection);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.Add("P_SCLIENT", OracleDbType.Char, client.SClient, ParameterDirection.Input);
+                command.Parameters.Add("P_DBIRTHDAT", OracleDbType.Date, client.DBirthDat, ParameterDirection.Input);
+                command.Parameters.Add("P_SFIRSTNAME", OracleDbType.Char, client.SFirstName, ParameterDirection.Input);
+                command.Parameters.Add("P_SLASTNAME", OracleDbType.Char, client.SLastName, ParameterDirection.Input);
+                command.Parameters.Add("P_SLASTNAME2", OracleDbType.Char, client.SLastName2, ParameterDirection.Input);
+                command.Parameters.Add("P_SCUIT", OracleDbType.Char, client.SCuit, ParameterDirection.Input);
+                command.Parameters.Add("P_SLEGALNAME", OracleDbType.Char, client.SLegalName, ParameterDirection.Input);
+                command.Parameters.Add("P_SSEXCLIEN", OracleDbType.Char, client.SSexClien, ParameterDirection.Input);
+                command.Parameters.Add("P_NNATIONALITY", OracleDbType.Int32, client.NNationality, ParameterDirection.Input);
                 command.ExecuteNonQuery();
                 return Task.FromResult(true);
             }
             catch (Exception ex)
             {
-                // Manejo de excepciones, loguear el error o lanzar una excepción personalizada
                 throw new Exception("Error al crear el cliente juridico", ex);
             }
+            finally
+            {
+                context.Database.CloseConnection();
+            }
+
 
         }
 
